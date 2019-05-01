2e081c4ab8f9037b061f1baf3822524d4e9bb207
Matt Huston <matthuston1@aol.com>
2019-05-01 14:30:54 -0600

    added slideshow to about page

---
+++ app.js
@@ -0,0 +1,15 @@
+var x = document.getElementsByClassName("mySlides")
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1} 
+  x[slideIndex-1].style.display = "block"; 
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
