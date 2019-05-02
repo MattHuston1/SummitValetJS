e26ad76eb37105486b71a44373d1738849bb230a
Matt Huston <matthuston1@aol.com>
2019-05-02 10:27:43 -0600

    added slideshow to locations page

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var x = document.getElementsByClassName("mySlides")
-
+var y = document.getElementsByClassName("mySlides2")
+var z = document.getElementsByClassName("mySlides3")
 var slideIndex = 0;
 carousel();
+// carousel2();
+carousel3();
 
 function carousel() {
   var i;
@@ -12,4 +15,26 @@ function carousel() {
   if (slideIndex > x.length) {slideIndex = 1} 
   x[slideIndex-1].style.display = "block"; 
   setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
+// function carousel2() {
+//   var i;
+//   for (i = 0; i < y.length; i++) {
+//     y[i].style.display = "none"; 
+//   }
+//   slideIndex++;
+//   if (slideIndex > y.length) {slideIndex = 1} 
+//   y[slideIndex-1].style.display = "block"; 
+//   setTimeout(carousel2, 2000); // Change image every 2 seconds
+// }
+
+function carousel3() {
+  var i;
+  for (i = 0; i < z.length; i++) {
+    z[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > z.length) {slideIndex = 1} 
+  z[slideIndex-1].style.display = "block"; 
+  setTimeout(carousel3, 2000); // Change image every 2 seconds
 }
