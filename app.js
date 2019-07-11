c45b131a4d8d05755ac6b35bb8965bff9a36cc8e
Matt Huston <matthuston1@aol.com>
2019-07-11 17:41:20 -0600

    changed order on js page because slideshow wasn't working

--- app.js
+++ app.js
@@ -1,35 +1,3 @@
-$(document).ready(function () {
-  $(".submit").click(function (event) {
-    // event.preventDefault()
-    console.log('Clicked')
-
-    var email = $(".email").val()
-    var subject = $(".subject").val()
-    var message = $(".message").val()
-    var status = $(".status")
-    status.empty()
-
-    if(email.length >= 5 && email.includes("@") && email.includes(".")) {
-      status.append('<div>Email is valid</div>')
-    } else {
-      event.preventDefault()
-      status.append('<div>Email is not valid</div>')
-    }
-    if(subject.length >= 2) {
-      status.append('<div>Subject is valid</div>')
-    } else {
-      event.preventDefault()
-      status.append('<div>Subject is not valid</div>')
-    }
-    if(message.length >= 10) {
-      status.append('<div>Message is valid</div>')
-    } else {
-      event.preventDefault()
-      status.append('<div>Message is not valid</div>')
-    }
-  })
-})
-
 var x = document.getElementsByClassName("mySlides")
 var y = document.getElementsByClassName("mySlides2")
 var z = document.getElementsByClassName("mySlides3")
@@ -50,30 +18,59 @@ function carousel() {
 }
 
 // function carousel2() {
-//   var i;
-//   for (i = 0; i < y.length; i++) {
-//     y[i].style.display = "none"; 
-//   }
-//   slideIndex++;
-//   if (slideIndex > y.length) {slideIndex = 1} 
-//   y[slideIndex-1].style.display = "block"; 
-//   setTimeout(carousel2, 2000); // Change image every 2 seconds
-// }
-
-function carousel3() {
-  var i;
-  for (i = 0; i < z.length; i++) {
-    z[i].style.display = "none"; 
-  }
-  slideIndex++;
-  if (slideIndex > z.length) {slideIndex = 1} 
-  z[slideIndex-1].style.display = "block"; 
-  setTimeout(carousel3, 2000); // Change image every 2 seconds
-}
-
+  //   var i;
+  //   for (i = 0; i < y.length; i++) {
+    //     y[i].style.display = "none"; 
+    //   }
+    //   slideIndex++;
+    //   if (slideIndex > y.length) {slideIndex = 1} 
+    //   y[slideIndex-1].style.display = "block"; 
+    //   setTimeout(carousel2, 2000); // Change image every 2 seconds
+    // }
+    
+    function carousel3() {
+      var i;
+      for (i = 0; i < z.length; i++) {
+        z[i].style.display = "none"; 
+      }
+      slideIndex++;
+      if (slideIndex > z.length) {slideIndex = 1} 
+      z[slideIndex-1].style.display = "block"; 
+      setTimeout(carousel3, 2000); // Change image every 2 seconds
+    }
 $(document).ready(function () {
   $(".submit").click(function (event) {
-    event.preventDefault()
+    // event.preventDefault()
     console.log('Clicked')
+
+    var email = $(".email").val()
+    var subject = $(".subject").val()
+    var message = $(".message").val()
+    var status = $(".status")
+    status.empty()
+
+    if(email.length >= 5 && email.includes("@") && email.includes(".")) {
+      // status.append('<div>Email is valid</div>')
+      console.log('Email is valid')
+    } else {
+      event.preventDefault()
+      status.append('<div>Email is not valid</div>')
+    }
+    if(subject.length >= 2) {
+      // status.append('<div>Subject is valid</div>')
+      console.log('Subject is valid')
+    } else {
+      event.preventDefault()
+      status.append('<div>Subject is not valid</div>')
+    }
+    if(message.length >= 10) {
+      // status.append('<div>Message is valid</div>')
+      console.log('Message is valid')
+    } else {
+      event.preventDefault()
+      status.append('<div>Message is not valid</div>')
+    }
   })
 })
+  
+  
